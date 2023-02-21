50067cfef7373eea295e49af450df5e0a2f0be46
bruce <bruce>
2023-02-21 18:17:43 +0800

    add io and math

---
+++ 29.math.py
@@ -0,0 +1,17 @@
+import math
+
+print(dir(math))
+
+print("============ log ============")
+print(math.log2(32))  # 5，回傳浮點數
+print(math.log10(100))  # 2，回傳浮點數
+print(math.log(32, 2))  # 5，回傳浮點數
+print(math.log(100, 10))  # 2，回傳浮點數
+
+print("============ 其他 ============")
+print(math.pi)
+print(math.fabs(-99))  # 絕對值，回傳浮點數
+print(math.e)
+print(math.fmod(10, 3))  # 回傳浮點數
+print(math.pow(2, 5))  # 回傳浮點數
+print(math.sqrt(9))  # 根號，回傳浮點數
