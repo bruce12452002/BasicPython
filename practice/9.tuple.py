a5f0c40ed34c4aa43bad71eeb06798c6776ab673
bruce <bruce>
2023-02-26 15:20:07 +0800

    update question

--- practice/9.tuple.py
+++ practice/9.tuple.py
@@ -1,8 +1,8 @@
 # 1.倒序輸出 my_tuple 的內容
 my_tuple = (7, 8, 9, 10)
 
-# 2.以下的 tuple 少了 b，請將 b 插入正確位置
-
+# 2.以下的 abcde_tuple 少了元素 b，請將 b 插入正確位置
+abcde_tuple = ('a', "c", 'd', 'e')
 
 # 3.承上題，取得 abcde_tuple 元素裡， d 的索引值
 
