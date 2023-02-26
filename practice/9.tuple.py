ed31adc411d355bdb6221b728f12ad3ad6f6744f
bruce <bruce>
2023-02-26 14:57:12 +0800

    add practice

--- practice/9.tuple.py
+++ practice/9.tuple.py
@@ -1,3 +1,11 @@
-# 倒序輸出 my_tuple 的內容
+# 1.倒序輸出 my_tuple 的內容
 my_tuple = (7, 8, 9, 10)
 
+# 2.以下的 tuple 少了 b，請將 b 插入正確位置
+
+
+# 3.承上題，取得 abcde_tuple 元素裡， d 的索引值
+
+
+# 4.輸出 tup 裡的元素 c 有幾個
+tup = ('c', "a", "b", 'c', False, 'c', 56)
