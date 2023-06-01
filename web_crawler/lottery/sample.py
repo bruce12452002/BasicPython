911cbca1ee0111472c60489f071e653d3d761e96
bruce <bruce>
2023-06-01 15:58:10 +0800

    update crawler

--- web_crawler/lottery/sample.py
+++ web_crawler/lottery/sample.py
@@ -9,16 +9,26 @@ res = req.get("https://www.taiwanlottery.com.tw")
 print("============ BeautifulSoup ============")
 bs = BeautifulSoup(res.text, "lxml")
 
-power = bs.find('div', class_='contents_box02')
+game_index = 3  # 0:威力彩  1:38樂合彩  2:大樂透  3:49樂合彩
+power = bs.find_all('div', class_='contents_box02')
 print('power==>', power)
-title = power.find('span', class_='font_black15').text
+title = power[game_index].find('span', class_='font_black15').text
 print('title==>', title)
 
-datas = power.find_all('div', class_='ball_tx ball_green')
+match game_index:
+    case 0 | 1:
+        color = 'ball_green'
+    case 2 | 3:
+        color = 'ball_yellow'
+    case _:
+        color = ''
+
+datas = power[game_index].find_all('div', class_='ball_tx ' + color)
 print('datas==>', datas)
 
 for i in range(0, 6):
     print(datas[i].text, end=' ')
+print()
 for i in range(6, 12):
     print(datas[i].text, end=' ')
 print('==========')
@@ -33,4 +43,7 @@ print('==========')
 # print(machine_order_list)
 # print(asc_order_list)
 
-print('第二區:', power.find('div', class_='ball_red').text)
+if game_index == 0:
+    print('第二區:', power[game_index].find('div', class_='ball_red').text)
+elif game_index == 2:
+    print('特別號:', power[game_index].find('div', class_='ball_red').text)
