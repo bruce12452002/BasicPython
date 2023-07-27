ef14997a821aab52e6275e6ad0c710a8497d6922
bruce <bruce>
2023-07-27 14:46:44 +0800

    add loc method

--- my_pandas/1.read.py
+++ my_pandas/1.read.py
@@ -13,13 +13,20 @@ print(c.dtypes)  # 每一欄的資料類型
 print("============ read txt ============")
 # header=None 表示將標題變成內容；names 表示自訂標籤名稱，沒指定是左到右的索引
 # 有 names 時，header 會自動 None
-t = pd.read_csv("./txt_file.txt", sep="--", header=None, names=['xxx', 'ooo', 'qoo'])
+t = pd.read_csv("./txt_file.txt", sep="--", header=None, names=['xxx', 'ooo', 'qoo'], engine='python')
 print(t.head())
 
 print("============ read other ============")
 # pd.read_excel("")
 
-import pymysql as mysql
+# import pymysql as mysql
+#
+# conn = mysql.connect(host='127.0.0.1', user='root', password='123456', database='my_db')
+# pd.read_sql("select * from table", con=conn)
 
-conn = mysql.connect(host='127.0.0.1', user='root', password='123456', database='my_db', charset='utf-8')
-pd.read_sql("select * from table", con=conn)
+print("============ set_index ============")
+print(c.head())
+print(c.index)
+c.set_index("姓名", inplace=True)
+print(c.index)
+print(c.head())
---
+++ my_pandas/3.data_frame.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+print("============ DataFrame ============")
+# DataFrame 相當於多維陣列
+
+c = pd.read_csv("./csv_file.csv")
+print(c['姓名'], type(c['姓名']))  # 一個欄位回傳 Series
+print(c[['姓名', '電話']], type(c[['姓名', '電話']]))  # 多個欄位回傳 DataFrame
+
+print("============ 查一行 ============")
+print(c.loc[1], type(c.loc[1]))  # 查第 1 行，從 0 開始，是 Series 型態
+
+print("============ 查多行 ============")
+print(c.loc[1:3], type(c.loc[1:3]))  # 這裡和 list 不太一樣，會包括最後的數字，是 DataFrame 型態
+
+
+print("============ update1 ============")
+c.set_index('姓名', inplace=True)
+print(c.loc[:, '電話'])
+c.loc[:, '電話'] = c['電話'] + 1000  # 數字增減
+print(c.head())
+print(c.dtypes)
+
+print("============ update2 ============")
+w = pd.read_csv("./weather.csv")
+print(w.head())
+w.set_index('ymd', inplace=True)
+print(w.loc[:, 'high'])  # index 不能設定
+w.loc[:, 'high'] = w['high'].str.replace('°C', '')  # 字串增減
+w.loc[:, 'low'] = w['low'].str.replace('°C', '').astype('int32')  # 字串增減 TODO astype 轉換型態失敗
+print(w.dtypes)
+print(w.head())
+
+print("============ 精確查詢 ============")
+print(w.loc[['2022-01-01', '2022-01-03', '2022-01-05'], 'high'])
+print(w.loc[['2022-01-01', '2022-01-03', '2022-01-05'], ['low', 'high']])
+
+print("============ 範圍查詢 ============")
+print(w.loc['2022-01-03': '2022-01-05', 'high'])
+
+print("============ 條件查詢 ============")
+print(c.loc[c['電話'] > 10000, :])
