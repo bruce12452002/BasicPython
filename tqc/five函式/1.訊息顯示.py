6c93e63b3d038f55e55d70ff2169514f16553182
bruce <bruce>
2023-10-13 00:44:26 +0800

    rename folder

---
+++ tqc/five函式/1.訊息顯示.py
@@ -0,0 +1,13 @@
+"""
+請撰寫一程式，呼叫函式compute()，該函式功能為讓使用者輸入系別（Department）、
+學號（Student ID）和姓名（Name）並顯示這些訊息
+
+範例輸入
+Information Management
+123456789
+Tina Chen
+範例輸出
+Department: Information Management
+Student ID: 123456789
+Name: Tina Chen
+"""
