2208126d0c37ea89f5de84f67ede1fb644799773
bruce12452002 <bruce12452002@gmail.com>
2023-10-23 14:42:07 +0800

    701 702 solution

--- tqc/seven元組集合字典/1.串列數組轉換.py
+++ tqc/seven元組集合字典/1.串列數組轉換.py
@@ -1,6 +1,7 @@
 """
 請撰寫一程式，輸入數個整數並儲存至串列中，以輸入-9999為結束點（串列中不包含-9999），
-再將此串列轉換成數組，最後顯示該數組以及其長度（Length）、最大值（Max）、最小值（Min）、總和（Sum）
+再將此串列轉換成數組，最後顯示該數組以及其長度（Length）、
+最大值（Max）、最小值（Min）、總和（Sum）
 
 範例輸入
 -4
@@ -18,3 +19,15 @@ Max: 37
 Min: -43
 Sum: 44
 """
+my_list = []
+while True:
+    n = int(input("請輸入整數:"))
+    if n == -9999:
+        break
+    my_list.append(n)
+
+print(tuple(my_list))
+print(f"Length: {len(my_list)}")
+print(f"Max: {max(my_list)}")
+print(f"Min: {min(my_list)}")
+print(f"Sum: {sum(my_list)}")
--- tqc/seven元組集合字典/2.數組合併排序.py
+++ tqc/seven元組集合字典/2.數組合併排序.py
@@ -1,5 +1,5 @@
 """
-請撰寫一程式，輸入並建立兩組數組，各以-9999為結束點（數組中不包含-9999）。
+請撰寫一程式，輸入並建立兩組數組，各以-9999為結束點（數組中不包含-9999）
 將此兩數組合併並從小到大排序之，顯示排序前的數組和排序後的串列
 
 輸入與輸出會交雜如下，輸出的部份以粗體字表示
@@ -21,3 +21,26 @@ Create tuple2:
 Combined tuple before sorting: (9, 0, -1, 3, 8, 28, 16, 39, 56, 78, 88)
 Combined list after sorting: [-1, 0, 3, 8, 9, 16, 28, 39, 56, 78, 88]
 """
+my_list1 = []
+my_list2 = []
+
+i = 0
+while i < 2:
+    print(f"Create tuple{i + 1}:")
+    n = int(input('請輸入整數:'))
+
+    if n == -9999:
+        if i == 1:
+            break
+        i += 1
+        continue
+
+    if i == 0:
+        my_list1.append(n)
+    if i == 1:
+        my_list2.append(n)
+
+my_list1.extend(my_list2)
+print("Combined tuple before sorting:{}".format(tuple(my_list1)))
+my_list1.sort()
+print("Combined list after sorting:{}".format(my_list1))
