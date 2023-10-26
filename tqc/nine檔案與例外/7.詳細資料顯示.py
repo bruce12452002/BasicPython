680cd68768d185e6d794d37b8a690553191b0fcb
bruce <bruce>
2023-10-26 14:32:49 +0800

    add 907 solution

--- tqc/nine檔案與例外/7.詳細資料顯示.py
+++ tqc/nine檔案與例外/7.詳細資料顯示.py
@@ -1,14 +1,16 @@
 """
-請撰寫一程式，要求使用者輸入檔名read.txt，顯示該檔案的行數、單字數（簡單起見，單字以空白隔開即可，忽略其它標點符號）以及字元數（不含空白）
+請撰寫一程式，要求使用者輸入檔名read.txt，顯示該檔案的行數、單字數
+（簡單起見，單字以空白隔開即可，忽略其它標點符號）以及字元數（不含空白）
 
 範例輸入
 read.txt
+
 範例輸出
 6 line(s)
 102 word(s)
 614 character(s)
-read.txt檔案內容
 
+read.txt檔案內容
 What is Python language?
 Python is a widely used high-level, general-purpose, interpreted, dynamic programming language.
 Its design philosophy emphasizes code readability, and its syntax allows programmers to express concepts in fewer lines of code than possible in languages such as C++ or Java.
@@ -16,3 +18,13 @@ Python supports multiple programming paradigms, including object-oriented, imper
 It features a dynamic type system and automatic memory management and has a large and comprehensive standard library.
 The best way we learn anything is by practice and exercise questions. We have started this section for those (beginner to intermediate) who are familiar with Python.
 """
+with open("read907.txt", mode="r") as f:
+    line = word = char = 0
+    for i in f.readlines():
+        line += 1
+        w = i.split()
+        word += len(w)
+        char += sum(len(i) for i in w)
+    print(f"{line} line(s)")
+    print(f"{word} word(s)")
+    print(f"{char} character(s)")
