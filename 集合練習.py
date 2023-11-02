0ef7dfc95cd4ed11ce078fbdf388d48fe9e599b1
bruce <bruce>
2023-11-02 23:49:00 +0800

    update code

--- 集合練習.py
+++ 集合練習.py
@@ -6,7 +6,7 @@ with open("集合.txt", encoding="UTF-8") as f:
         my_list.append(data.strip().split(","))
 
 while True:
-    n = r.randrange(0, len(my_list))
+    n = r.randrange(len(my_list))
     keyIn = input(my_list[n][0] + ": ")
     if keyIn.strip().lower() != my_list[n][1].strip().lower():
         print(f"答錯了！正確答案是: {my_list[n][1].strip()}")
