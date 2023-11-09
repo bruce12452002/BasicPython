ad5694f60ee005200483943637243a58b90d1026
bruce <bruce>
2023-11-09 15:48:21 +0800

    modify generator

--- oop/12.generator.py
+++ oop/12.generator.py
@@ -1,7 +1,33 @@
-def display(data):
-    for index in range(0, len(data), 1):
-        yield data[index]  # generator 用 yield 回傳
+a = [i ** 2 for i in range(10)]
+b = (i ** 2 for i in range(10))
+print(type(a), type(b))
+for i in b:
+    print(i, end=' ')  # 0 1 4 9 16 25 36 49 64 81
+for i in b:
+    print(i, end=' ')  # 只能跑一次，所以這個迴圈沒跑
+
+print("============ next 取值 ============")
+b = (i ** 2 for i in range(10))
+# 每次跑一個值，如果想全跑，可用迴圈
+print(next(b))  # 0
+print(next(b))  # 1
+print(next(b))  # 4
+
+print("============ yield 寫在方法裡 ============")
 
 
-for c in display("abc"):
-    print(c)
+def f():
+    yield 1  # 使用 yield
+    yield 2
+    yield 3
+
+
+g = f()  # 賦值給變數 g
+print(next(g))  # 1
+print(next(g))  # 2
+print(next(g))  # 3
+# print(next(g))  # 報錯
+
+print(next(f()))  # 1
+print(next(f()))  # 1
+print(next(f()))  # 1
