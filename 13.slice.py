5e8f9a8df69ec5d8607284ceb5155aed2e9ad52b
bruce <bruce>
2026-04-13 14:19:44 +0800

    add sample

--- 13.slice.py
+++ 13.slice.py
@@ -6,7 +6,6 @@
 my_tuple = ("a", "b", "c", "d", "e", "f")
 # print(my_tuple[0:len(my_tuple):1]) # 正序
 # print(my_tuple[-1:-len(my_tuple)-1:-1]) # 倒序
-
 print(my_tuple[1:5])  # b~e
 print(my_tuple[-2:-1])  # e
 print(my_tuple[-1])  # f
@@ -17,10 +16,11 @@ print(my_tuple[:])  # 全部
 print(my_tuple[0:])  # 全部
 print(my_tuple[0::])  # 全部
 print(my_tuple[0::1])  # 全部
+print(my_tuple[1:-2:]) # bcd，從 1 開始取，而 -2 也是 4
 print(my_tuple[::-1])  # 負數在最後面是倒著取
 print(my_tuple[4:1:-1])  # edc
 print(my_tuple[-2:-6:-1]) # edcb
-print(my_tuple[1:-7:-1]) # ba，從 1 開始取，而1也是-5，然後往前取到 a
+print(my_tuple[1:-7:-1]) # ba，從 1 開始取，而1也是 -5，然後往前取到 a
 print(my_tuple[::-1][0:2])  # 倒著取之後再取前兩個，所以是 fe
 
 
